61a575451d5784f953b883a3782ef66e8ebf9ca2
Teorell <fedorizmalkov@gmail.com>
2021-10-23 22:04:13 +0300

    tried webclient

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,11 +3,13 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,10 +19,15 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        RestTemplate restTemplate = new RestTemplate();
+        WebClient.Builder builder = WebClient.builder();
 
         // get all rated movie IDs
         List<Rating> ratings = Arrays.asList(
@@ -29,15 +36,23 @@ public class MovieCatalogResource {
         );
 
         return ratings.stream().map(rating -> {
+            // done by restTemplate
             ResponseEntity<Movie> movieResponseEntity = restTemplate.getForEntity("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
             Movie movie = movieResponseEntity.getBody();
+
+            // using webClient
+//            Movie movie = webClientBuilder.build()
+//                    .get()
+//                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+//                    .retrieve()
+//                    .bodyToMono(Movie.class)
+//                    .block();
+
             return new CatalogItem(movie.getName(), "Test desc", rating.getRating());
         }).collect(Collectors.toList());
 
-        //for each ID call movie info service and get details
 
-        // put them all together
-
+        // mock
 //        return Collections.singletonList(
 //                new CatalogItem("Transformers", "Test", 4)
 //        );
