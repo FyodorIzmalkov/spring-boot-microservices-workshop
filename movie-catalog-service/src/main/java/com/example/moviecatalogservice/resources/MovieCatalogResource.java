bfbbd15da25fa5f48fd7b32336d918100b102368
Teorell <fedorizmalkov@gmail.com>
2021-10-24 20:13:50 +0300

    wrapper added

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,15 +3,14 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.Rating;
+import com.example.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,26 +20,24 @@ public class MovieCatalogResource {
 
     @Autowired
     private RestTemplate restTemplate;
-    @Autowired
-    private WebClient.Builder webClientBuilder;
 
+    // should actually return wrapper instead of a list
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        WebClient.Builder builder = WebClient.builder();
-
-        // get all rated movie IDs
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        List<Rating> ratings = userRating.getUserRating();
 
         return ratings.stream().map(rating -> {
-            // done by restTemplate
             ResponseEntity<Movie> movieResponseEntity = restTemplate.getForEntity("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
             Movie movie = movieResponseEntity.getBody();
 
-            // using webClient
+            return new CatalogItem(movie.getName(), "Test desc", rating.getRating());
+        }).collect(Collectors.toList());
+    }
+}
+
+// using webClient
 //            Movie movie = webClientBuilder.build()
 //                    .get()
 //                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
@@ -48,13 +45,7 @@ public class MovieCatalogResource {
 //                    .bodyToMono(Movie.class)
 //                    .block();
 
-            return new CatalogItem(movie.getName(), "Test desc", rating.getRating());
-        }).collect(Collectors.toList());
-
-
-        // mock
+// mock
 //        return Collections.singletonList(
 //                new CatalogItem("Transformers", "Test", 4)
 //        );
-    }
-}
